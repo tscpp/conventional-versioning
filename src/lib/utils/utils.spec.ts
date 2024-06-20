6c864499061122f64f379f8136aa697ab9583f59
Elias Skogevall <elias.skogevall@gmail.com>
2024-06-20 14:02:10 +0200

    test: fix tests

--- src/lib/utils/utils.spec.ts
+++ src/lib/utils/utils.spec.ts
@@ -1,6 +1,8 @@
 import { describe, expect, it } from "@jest/globals";
 import { containsPath } from "./utils.js";
 
+const skipIf = (condition: unknown) => (condition ? it.skip : it);
+
 describe("containsPath", () => {
   it("returns true for file under directory", () => {
     const value = containsPath("/home/elias", "/home/elias/file.txt");
@@ -17,8 +19,11 @@ describe("containsPath", () => {
     expect(value).toBe(true);
   });
 
-  it("works with backslashes", () => {
-    const value = containsPath("C:\\Users\\elias", "C:\\Users\\elias\\file.txt");
+  skipIf(process.platform !== "win32")("works with backslashes", () => {
+    const value = containsPath(
+      "C:\\Users\\elias",
+      "C:\\Users\\elias\\file.txt"
+    );
     expect(value).toBe(true);
   });
 });

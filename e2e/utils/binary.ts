16ca4142792e46e64ca63d50f547775098783207
Elias Skogevall <elias.skogevall@gmail.com>
2024-06-21 11:51:56 +0200

    test: fix binary file permissions

--- e2e/utils/binary.ts
+++ e2e/utils/binary.ts
@@ -1,4 +1,9 @@
 import { resolve } from "node:path";
 import packageJson from "../../package.json";
+import { $ } from "execa";
 
 export const CONVER = resolve(packageJson.bin.conver);
+
+if (process.platform !== "win32") {
+  await $`chmod +x ${CONVER}`;
+}
